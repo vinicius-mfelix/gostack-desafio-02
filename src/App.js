06fdb1ef02f38bddbe1a9c891338b500aad7a867
vinicius-mfelix <vinicius.mfelix@icloud.com>
2020-11-09 00:00:54 -0300

    Implementacao dos metodos HTTP com backend utilizando Hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('/repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const repo = {
+      title: "flutter",
+      url: "https://github.com/flutter/flutter",
+      techs: [
+        "Dart"
+      ]
+    };
+
+    const response = await api.post('/repositories', repo);
+
+    setRepositories([...repositories, response.data]);
+
   }
 
-  async function handleRemoveRepository(id) {
-    // TODO
+  function handleRemoveRepository(id) {
+    api.delete('/repositories/' + id).then(() => {
+      const repos = repositories.filter(repo => repo.id !== id);
+      setRepositories(repos);
+    });
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {
+          repositories.map(repo => repo.id !== undefined &&
+            <li key={repo.id}>{repo.title}
+              <button onClick={() => handleRemoveRepository(repo.id)}>
+                Remover
+              </button>
+            </li>
+          )
+        }
+        
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
